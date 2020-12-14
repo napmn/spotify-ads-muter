dcfc7da582fc08c27274f1acdddd71e641eac280
napmn <lukass135@gmail.com>
2020-12-14 18:27:39 +0100

    :rocket: version 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spotify ads muter
+
+Sets Spotify volume to zero when an ad is detected. Brings volume back up after ad is finished.
---
+++ spotify_ads_muter.py
@@ -0,0 +1,42 @@
+import math
+import time
+
+import pync
+from wasp_spotify_bindings.core import Wasp, WaspException
+
+
+def check_for_ads(spotify: Wasp):
+    try:
+        track = spotify.get_track()
+        state = spotify.get_state()
+    except WaspException:
+        # spotify is not running
+        time.sleep(10)
+        return
+
+    duration_sec = track['duration'] / 1000
+
+    if state['state'] == 'playing':
+        if track['name'] == 'Advertisement':
+            previous_volume = state['volume']
+            mute_duration = math.floor(duration_sec - state['position']) - 0.5
+            if mute_duration > 0:
+                # weird bug occured that sometimes duration was negative
+                pync.notify(
+                    f'Ad detected, muting spotify for {mute_duration} seconds',
+                    title='Spotify ads muter'
+                )
+                spotify.set_volume(0)
+                time.sleep(mute_duration)
+                spotify.set_volume(previous_volume)
+        else:
+            time.sleep(1)
+    else:
+        time.sleep(5)
+
+
+if __name__ == '__main__':
+    spotify = Wasp()
+
+    while True:
+        check_for_ads(spotify)
